e32377f6c0272aaefeeb7f5a047bc1d0d2a95dd7
Erik Wogstad <ewogstad@gmail.com>
2011-11-26 12:41:15 -0800

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,6 +41,25 @@ describe UsersController do
       get :new
       response.should have_selector("title", :content => "Sign up")
     end
+
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
+
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type ='password']")
+    end
   end
 
 
@@ -57,7 +76,7 @@ describe UsersController do
       end
       it "should have the right title" do
         post :create, :user => @attr
-        response.should_have_selector("title", :content => "Sign up")
+        response.should have_selector("title", :content => "Sign up")
       end
       it "should render the 'new' page" do
         post :create, :user => @attr
